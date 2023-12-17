f4a553e9418885ed02a099c2de31037764423191
nicksobolev <nikitasob23@gmail.com>
2023-12-17 21:37:37 +0300

    The DomainApplication has been removed as the domain module is not used independently, which means it does not require a Spring Boot main class. The removal of DomainApplication was necessary because the domain module is not used independently, and thus, it doesn't require a Spring Boot main class. However, Spring DI (Dependency Injection) won't function without a main class, as it plays a crucial role in automatically locating the Spring context for the domain. To address this, I added the MainDomainConfig configuration and then provided the Spring context for the entire module using @ComponentScan.
    Removed unnecessary @Accessors from UserInfo.

---
+++ domain/src/main/java/com/niksob/domain/config/MainDomainConfig.java
@@ -0,0 +1,9 @@
+package com.niksob.domain.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.niksob.domain")
+public class MainDomainConfig {
+}
--- domain/src/main/java/com/niksob/domain/model/user/UserInfo.java
+++ domain/src/main/java/com/niksob/domain/model/user/UserInfo.java
@@ -3,12 +3,10 @@ package com.niksob.domain.model.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Accessors(chain = true)
 public class UserInfo {
     private Username username;
     private Nickname nickname;
