469484f6cf17303e08ef8a9c776041062a0cce24
nicksobolev <nikitasob23@gmail.com>
2024-02-22 15:09:19 +0300

    Added start state of authorization microservice module

---
+++ authorization-service/src/test/java/org/niksob/authorization_service/AuthorizationMicroserviceTests.java
@@ -0,0 +1,13 @@
+package org.niksob.authorization_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationMicroserviceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
