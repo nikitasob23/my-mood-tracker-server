4baed04890d7e08d1990470b31d2c1be53157cb7
nicksobolev <nikitasob23@gmail.com>
2024-03-13 00:03:43 +0300

    Renamed authorization static path from /api/service to /api/service/auth

--- domain/src/main/java/com/niksob/domain/path/microservice/MicroservicePath.java
+++
@@ -1,5 +0,0 @@
-package com.niksob.domain.path.microservice;
-
-public class MicroservicePath {
-    public static final String AUTH = "auth";
-}
--- domain/src/main/java/com/niksob/domain/path/microservice/MicroservicePorts.java
+++
@@ -1,6 +0,0 @@
-package com.niksob.domain.path.microservice;
-
-public class MicroservicePorts {
-    public static final int DATABASE = 8081;
-    public static final int AUTH = 8092;
-}
