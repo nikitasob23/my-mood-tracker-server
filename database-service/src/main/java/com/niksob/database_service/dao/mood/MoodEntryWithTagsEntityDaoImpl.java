8278c444402acc09f9c3ff0a58b733fb5db046b2
nicksobolev <nikitasob23@gmail.com>
2024-04-12 16:54:03 +0300

    Fix bug with tags duplication in mood state when returning the full user.

--- database-service/src/main/java/com/niksob/database_service/dao/mood/MoodEntryWithTagsEntityDaoImpl.java
+++ database-service/src/main/java/com/niksob/database_service/dao/mood/MoodEntryWithTagsEntityDaoImpl.java
@@ -9,12 +9,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class MoodEntryWithTagsEntityDaoImpl extends MoodEntryEntityDaoImpl implements MoodEntryWithTagsEntityDao {
     private final TagEntityDaoFacade tagEntityDaoFacade;
 
-    public MoodEntryWithTagsEntityDaoImpl(TagEntityDaoFacade tagEntityDaoFacade, MoodEntryEntityRepository moodEntryRepository) {
+    public MoodEntryWithTagsEntityDaoImpl(
+            TagEntityDaoFacade tagEntityDaoFacade, MoodEntryEntityRepository moodEntryRepository
+    ) {
         super(moodEntryRepository);
         this.tagEntityDaoFacade = tagEntityDaoFacade;
     }
@@ -22,16 +25,30 @@ public class MoodEntryWithTagsEntityDaoImpl extends MoodEntryEntityDaoImpl imple
     @Override
     @Transactional
     public MoodEntryEntity saveEntryWithTags(MoodEntryEntity moodEntry) {
-        final Set<MoodTagEntity> moodTags = tagEntityDaoFacade.mergeAll(moodEntry.getMoodTags());
-        moodEntry.setMoodTags(moodTags);
+        final Set<MoodTagEntity> allUserTags = tagEntityDaoFacade.mergeAll(moodEntry.getMoodTags());
+
+        final Set<MoodTagEntity> entryTags = filterMoodTagsByEntry(allUserTags, moodEntry);
+        moodEntry.setMoodTags(entryTags);
         return super.save(moodEntry);
     }
 
     @Override
     @Transactional
     public void updateEntryWithTags(MoodEntryEntity moodEntry) {
-        final Set<MoodTagEntity> moodTags = tagEntityDaoFacade.mergeAll(moodEntry.getMoodTags());
-        moodEntry.setMoodTags(moodTags);
+        final Set<MoodTagEntity> allUserTags = tagEntityDaoFacade.mergeAll(moodEntry.getMoodTags());
+
+        final Set<MoodTagEntity> entryTags = filterMoodTagsByEntry(allUserTags, moodEntry);
+        moodEntry.setMoodTags(entryTags);
         super.update(moodEntry);
+    }
+
+    private Set<MoodTagEntity> filterMoodTagsByEntry(Set<MoodTagEntity> tags, MoodEntryEntity entry) {
+        final Set<String> entryTagNames = entry.getMoodTags().stream()
+                .map(MoodTagEntity::getName)
+                .collect(Collectors.toSet());
+        final Set<MoodTagEntity> entityTags = tags.stream()
+                .filter(mergedTag -> entryTagNames.contains(mergedTag.getName()))
+                .collect(Collectors.toSet());
+        return entityTags;
     }
 }
--- database-service/src/main/java/com/niksob/database_service/dao/mood/tag/facade/CachedTagDaoFacade.java
+++ database-service/src/main/java/com/niksob/database_service/dao/mood/tag/facade/CachedTagDaoFacade.java
@@ -4,16 +4,26 @@ import com.niksob.database_service.dao.mood.tag.loader.TagEntityLoaderDao;
 import com.niksob.database_service.dao.mood.tag.updater.TagEntityUpdaterDao;
 import com.niksob.database_service.entity.mood.tag.MoodTagEntity;
 import com.niksob.database_service.model.mood.tag.user.UserMoodTagEntities;
+import com.niksob.domain.exception.resource.ResourceSavingException;
+import com.niksob.domain.exception.resource.ResourceUpdatingException;
+import com.niksob.logger.object_state.ObjectStateLogger;
+import com.niksob.logger.object_state.factory.ObjectStateLoggerFactory;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
 public class CachedTagDaoFacade implements TagEntityDaoFacade {
     private final TagEntityLoaderDao loaderDao;
     private final TagEntityUpdaterDao updaterDao;
+
+    private final ObjectStateLogger log = ObjectStateLoggerFactory.getLogger(CachedTagDaoFacade.class);
 
     @Override
     public Set<MoodTagEntity> loadByUserId(Long userId) {
@@ -35,9 +45,27 @@ public class CachedTagDaoFacade implements TagEntityDaoFacade {
     }
 
     @Override
+    @Transactional
     public Set<MoodTagEntity> mergeAll(Set<MoodTagEntity> tags) {
-        final UserMoodTagEntities stored = updaterDao.mergeAll(tags);
-        return stored.getTags();
+        final Long userId = extractSingleUserId(tags);
+        final Set<Long> storedIds = loadByUserId(userId).stream()
+                .map(MoodTagEntity::getId)
+                .collect(Collectors.toSet());
+
+        final Set<Long> tagIds = tags.stream()
+                .map(MoodTagEntity::getId)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+
+        for (Long tagId : tagIds) {
+            if (!storedIds.contains(tagId)) {
+                final String message = "The tag that needs to be updated was not found";
+                log.error(message, null, tagId);
+                throw new ResourceSavingException(message, tagId, null);
+            }
+        }
+        final UserMoodTagEntities userMoodTagEntities = updaterDao.mergeAll(tags);
+        return userMoodTagEntities.getTags();
     }
 
     @Override
@@ -48,5 +76,22 @@ public class CachedTagDaoFacade implements TagEntityDaoFacade {
     @Override
     public void deleteAllByUserId(Long userId) {
         updaterDao.deleteAllByUserId(userId);
+    }
+
+    private Long extractSingleUserId(Collection<MoodTagEntity> moodTags) {
+        final Set<Long> userIds = moodTags.stream()
+                .map(MoodTagEntity::getUserId)
+                .collect(Collectors.toSet());
+        if (userIds.isEmpty()) {
+            log.error("Failed merging mood tag entities in repository", null, moodTags);
+            throw new ResourceUpdatingException(
+                    "Mood tag entities not merged because they haven't ids", null, userIds);
+        }
+        if (userIds.size() > 1) {
+            log.error("Failed merging mood tag entities in repository", null, moodTags);
+            throw new ResourceUpdatingException(
+                    "Mood tag entities not merged because they have different ids", null, userIds);
+        }
+        return userIds.iterator().next();
     }
 }
