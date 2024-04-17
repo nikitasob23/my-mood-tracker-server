42e74c5d561ee962856ce939ca99da2e545ae141
nicksobolev <nikitasob23@gmail.com>
2024-04-17 21:20:37 +0300

    Fixed a bug related to returning an internal server error when the access token validation fails. In this case needs to return forbidden.

--- gateway-service/src/main/java/com/niksob/gateway_service/authentication/manager/AccessTokenAuthenticationManager.java
+++ gateway-service/src/main/java/com/niksob/gateway_service/authentication/manager/AccessTokenAuthenticationManager.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -52,8 +53,8 @@ public class AccessTokenAuthenticationManager implements ReactiveAuthenticationM
     private <T> Mono<T> createAuthenticationMonoError(
             Throwable throwable, AuthenticationToken authenticationToken
     ) {
-        final String message = "Failure authentication by access token";
-        var e = new HttpClientException(message, HttpStatus.FORBIDDEN, authenticationToken.getPath());
+        final String message = "Failure authentication by access token, path: " + authenticationToken.getPath();
+        var e = new ResponseStatusException(HttpStatus.FORBIDDEN, message);
         log.error(message, throwable, authenticationToken);
         return Mono.error(e);
     }
