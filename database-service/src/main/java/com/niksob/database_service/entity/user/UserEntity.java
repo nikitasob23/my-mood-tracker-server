08834a860f4929897402a2169ef82f34a4e21fdb
nicksobolev <nikitasob23@gmail.com>
2024-04-28 22:36:14 +0300

    Fixed bug with user updating. When modifying a user, it was possible to add an empty email, which led to unforeseen application behavior. Because the database interaction was within a transaction, the transaction deferred the actual database access to the last moment. As a result, the exception thrown by the database was not caught by the try-catch block, but was thrown after the method had already completed. Because of this, the data in the database did not change, but it did change in the cache.

--- database-service/src/main/java/com/niksob/database_service/entity/user/UserEntity.java
+++ database-service/src/main/java/com/niksob/database_service/entity/user/UserEntity.java
@@ -26,6 +26,7 @@ public class UserEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
