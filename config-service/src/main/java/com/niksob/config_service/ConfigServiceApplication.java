a7a58acb7da3122692b1dde7e4f06742ebc4d359
nicksobolev <nikitasob23@gmail.com>
2024-05-14 00:23:42 +0300

    Added GitConfigLoader, which clone a git repository with all configurations. Based on all files, new ones are created with settings that are passed to microservices using standard spring cloud tools. New configuration files are created from settings specified in the repo.remote.git.properties-file config value.
    This approach was chosen due to the impossibility of passing settings from configuration files with names different from the standard spring cloud names.
    
    Annotations from ConfigServiceApplication have been moved to AppConfig.
    
    Добавлен GitConfigLoader, который копирует git репозиторий со всеми конфигурациями. Далее на основе всех файлов создаются новые с настройками, которые передаются микросервисам с помощью стандратных средств spring cloud. Новые файлы конфигурации создаются из настроек, указанных в repo.remote.git.properties-file с помощью
    
    Данных подход был выбран из-за невозможности передачи настроек из файлов конифгураций, отличных от стандратных названий spring cloud.
    
    Аннотации из ConfigServiceApplication были вынесены в AppConfig.

--- config-service/src/main/java/com/niksob/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/niksob/config_service/ConfigServiceApplication.java
@@ -2,10 +2,8 @@ package com.niksob.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
